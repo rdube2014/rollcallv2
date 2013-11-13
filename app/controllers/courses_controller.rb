a2bff6c48c1b2e481f21cd633b1feab03e54d7e1
Richard J Dube <rdube2014@kellogg.northwestern.edu>
2013-11-13 13:00:24 -0600

    11/13/2013 v5

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,7 +7,7 @@ class CoursesController < ApplicationController
   def show
     @course = Course.find_by(id: params[:id])
 
-    @meeting = Meeting.where(:course_id => "925")
+    @meeting = Meeting.where(:course_id => @course.number)
   end
 
   def new
